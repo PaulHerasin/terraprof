78bbb3e46ff7866ac5e9cd256f4a94128132d4de
Dima <timoha.lisyev2014@yandex.ru>
2021-07-04 16:30:20 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -69,7 +69,6 @@ import './assets/css/blocks/objects.scss';
 // CONTACT
 import './assets/css/blocks/map.scss';
 import './assets/css/blocks/contact.scss';
-import './assets/css/blocks/zoom.scss';
 
 
 
@@ -105,5 +104,4 @@ import './assets/js/process-robots';
 import './assets/js/types-robot';
 import './assets/js/objects';
 import './assets/js/map';
-import './assets/js/zoom';
 // import './assets/js/inputs-mask';
