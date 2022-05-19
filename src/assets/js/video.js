f88dde7cd19a987a8fb1a32619c0c5e2c8bd53ec
Dima <timoha.lisyev2014@yandex.ru>
2022-05-19 21:41:47 +0300

    fix video

--- src/assets/js/video.js
+++ src/assets/js/video.js
@@ -1,18 +1,18 @@
-// $(".video").fancybox({
-//     type: "iframe",
-// });
+$(".video").fancybox({
+    type: "iframe",
+});
 
-// let videos = document.querySelectorAll('.video');
-// for (let i = 0; i < videos.length; i++) {
-//     videos[i].addEventListener('mouseover', function () {
-//         let iframeSrc = this.querySelector('iframe').getAttribute('data-src');
-//         this.querySelector('iframe').src = iframeSrc;
-//     });
+let videos = document.querySelectorAll('.video-iframe');
+for (let i = 0; i < videos.length; i++) {
+    videos[i].addEventListener('mouseover', function () {
+        let iframeSrc = this.querySelector('iframe').getAttribute('data-src');
+        this.querySelector('iframe').src = iframeSrc;
+    });
 
-//     videos[i].addEventListener('mouseleave', function () {
-//         this.querySelector('iframe').src = "";
-//     });
-// }
+    videos[i].addEventListener('mouseleave', function () {
+        this.querySelector('iframe').src = "";
+    });
+}
 
 ////////////////
 // let videosImg = document.querySelectorAll('.video-img');
